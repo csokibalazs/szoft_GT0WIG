50c7848b0be258b2dbca38f266f0dad7595503c8
csokibalazs <balazs.csoknyai@stud.uni-corvinus.hu>
2024-02-22 16:33:28 +0100

    Add project files.

---
+++ Fibonacci/Sor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Sor
+    {
+        //ctor [tab][tab] hozzat létre a konstruktort
+  
+        //prop [tab][tab] létrhoz egy ilyet lent
+        public int Sorszám { get; set; }
+        public int Érték { get; set; }
+
+    }
+}
