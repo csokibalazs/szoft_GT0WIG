a48bf9587542bff1a4bc6258f1ed6c698d1657e1
csokibalazs <balazs.csoknyai@stud.uni-corvinus.hu>
2024-04-11 16:39:58 +0200

    gyak done

--- 7 gyak/Form1.Designer.cs
+++ 7 gyak/Form1.Designer.cs
@@ -28,35 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             dataGridView1 = new DataGridView();
+            nameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            populationDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            areaInSquareKmDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            countryDataBindingSource = new BindingSource(components);
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)countryDataBindingSource).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.AutoGenerateColumns = false;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(163, 156);
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { nameDataGridViewTextBoxColumn, populationDataGridViewTextBoxColumn, areaInSquareKmDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = countryDataBindingSource;
+            dataGridView1.Location = new Point(37, 44);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
             dataGridView1.Size = new Size(414, 243);
             dataGridView1.TabIndex = 0;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // populationDataGridViewTextBoxColumn
+            // 
+            populationDataGridViewTextBoxColumn.DataPropertyName = "Population";
+            populationDataGridViewTextBoxColumn.HeaderText = "Population";
+            populationDataGridViewTextBoxColumn.Name = "populationDataGridViewTextBoxColumn";
+            // 
+            // areaInSquareKmDataGridViewTextBoxColumn
+            // 
+            areaInSquareKmDataGridViewTextBoxColumn.DataPropertyName = "AreaInSquareKm";
+            areaInSquareKmDataGridViewTextBoxColumn.HeaderText = "AreaInSquareKm";
+            areaInSquareKmDataGridViewTextBoxColumn.Name = "areaInSquareKmDataGridViewTextBoxColumn";
+            // 
+            // countryDataBindingSource
+            // 
+            countryDataBindingSource.DataSource = typeof(CountryData);
+            // 
+            // textBox1
+            // 
+            textBox1.DataBindings.Add(new Binding("Text", countryDataBindingSource, "Name", true));
+            textBox1.Enabled = false;
+            textBox1.Location = new Point(561, 69);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(126, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.DataBindings.Add(new Binding("Text", countryDataBindingSource, "Population", true));
+            textBox2.Location = new Point(561, 135);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(126, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.DataBindings.Add(new Binding("Text", countryDataBindingSource, "AreaInSquareKm", true));
+            textBox3.Location = new Point(561, 216);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(126, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(551, 342);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 57);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)countryDataBindingSource).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn populationDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn areaInSquareKmDataGridViewTextBoxColumn;
+        private BindingSource countryDataBindingSource;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
     }
 }
--- 7 gyak/Form1.cs
+++ 7 gyak/Form1.cs
@@ -14,7 +14,8 @@ namespace _7_gyak
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = countryList;
+            countryDataBindingSource.DataSource = countryList;
+            dataGridView1.DataSource = countryDataBindingSource;
             StreamReader sr = new StreamReader("Countries.csv");
             var csv = new CsvReader(sr, CultureInfo.InvariantCulture);
             var tömb = csv.GetRecords<CountryData>();
@@ -22,6 +23,13 @@ namespace _7_gyak
             {
                 countryList.Add(item);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CountryEdit ce = new CountryEdit();
+            ce.CountryData = countryDataBindingSource.Current as CountryData;
+            ce.Show();
         }
     }
 }
